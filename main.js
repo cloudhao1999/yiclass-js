f0a88d9a812ef07710774793994ef10fd2f05e1a
cyh199910 <929682749@qq.com>
2020-10-08 17:58:01 +0800

    授权

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import {parse_data} from './utils'
-import {login} from './yiban'
+import {login,auth,getUncompletedList} from './yiban'
 
 // 获取之前填过的表单的信息，从而进行复用数据
 parse_data()
@@ -11,6 +11,17 @@ login().then((res)=>{
         console.log('帐号或密码错误,请确认账号密码密码无误后重试')
     }else{
         console.log('登录成功，进行二次校验')
+        auth().then(res=>{
+            const data_url = res.data.Data
+            console.log(data_url)
+            if(data_url === null){
+                console.log("授权过期")
+            }else{
+                console.log("授权成功")
+                getUncompletedList()
+            }
+        })
+        
     }
 })
 
--- yiban.js
+++ yiban.js
@@ -1,25 +1,54 @@
 import axios from 'axios'
+import { CSRF, HEADERS } from './constant'
 
 let access_token = ''
 let name = ''
+let phpSessionId = ''
 
-export async function login(){
+axios.defaults.withCredentials = true
+
+export async function login() {
     const params = {
-        "account": '易班账户',
+        "account": '账户',
         "ct": 2,
         "identify": 0,
         "v": "4.7.4",
-        "passwd": '易班的密码'
+        "passwd": '密码'
     }
 
-    const r = await axios.get('https://mobile.yiban.cn/api/v2/passport/login',{params})
-    if(r&&r['data']['response']=="100"){
+    const r = await axios.get('https://mobile.yiban.cn/api/v2/passport/login', { params })
+    console.log(r)
+    if (r && r['data']['response'] == "100") {
         access_token = r["data"]["data"]["access_token"]
         name = r["data"]["data"]["user"]["name"]
-        console.log(access_token,name)
+        console.log(access_token, name)
         return r
-    }else{
+    } else {
         console.log("null")
         return null
     }
+}
+
+export async function auth() {
+    const response = await axios.get(`http://f.yiban.cn/iapp/index?act=iapp7463&v=${access_token}`)
+    console.log(response.request.res.responseUrl)
+    const location = response.request.res.responseUrl
+    const verifyRequest = location.slice(39)
+    console.log(verifyRequest)
+    const res =await axios.get(`https://api.uyiban.com/base/c/auth/yiban?verifyRequest=${verifyRequest}&CSRF=${CSRF}`, {
+        headers: HEADERS
+    })
+    console.log(res)
+    const cookie = res.headers["set-cookie"][0]
+    console.log(cookie)
+    phpSessionId = cookie.split(";")[0]
+    console.log(phpSessionId)
+    return res.data;
+}
+
+export async function getUncompletedList(){
+    const res = await axios.get(`https://api.uyiban.com/officeTask/client/index/uncompletedList?CSRF=${CSRF}`, {
+        headers: { "Origin": "https://c.uyiban.com", "User-Agent": "yiban","Cookie":`${phpSessionId};csrf_token=aaabbb`}
+    })
+    console.log(res.data.data)
 }
